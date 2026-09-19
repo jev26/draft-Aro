0108db75864c47ba269c5762de6936942eb91b20
Jennifer Veillard <Jennifer.Veillard@unige.ch>
2026-09-19 22:21:41 +0200

    first commit code

---
+++ js/layout.js
@@ -0,0 +1,96 @@
+// ======================================
+// Charge un composant HTML dans une balise
+// Exemple : header ou footer
+// ======================================
+
+async function loadComponent(id, file) {
+
+  const element = document.getElementById(id);
+
+  if (!element) return null;
+
+  try {
+
+    const response = await fetch(file);
+
+    if (!response.ok) {
+      throw new Error(`Impossible de charger ${file}`);
+    }
+
+    element.innerHTML = await response.text();
+
+    return element;
+
+  } catch (error) {
+
+    console.error(error);
+
+    return null;
+
+  }
+}
+
+// ======================================
+// Initialisation du Header
+// ======================================
+
+async function initHeader() {
+
+  const header = await loadComponent(
+    "header",
+    "/components/header.html"
+  );
+
+  if (!header) return;
+
+  document
+    .getElementById("main-navbar")
+    ?.classList.add(header.dataset.navbar);
+
+  const logo = document.getElementById("navbar-logo");
+
+if (logo && header.dataset.logo) {
+  logo.setAttribute("src", header.dataset.logo);
+} else if (logo) {
+  logo.style.display = "none";
+}
+}
+
+// ======================================
+// Initialisation du Footer
+// ======================================
+
+async function initFooter() {
+
+  const footer = await loadComponent(
+    "footer",
+    "/components/footer.html"
+  );
+
+  if (!footer) return;
+
+  document
+    .querySelector(".back-to-top")
+    ?.addEventListener("click", function (e) {
+
+      e.preventDefault();
+
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+
+    });
+
+}
+
+// ======================================
+// Démarrage de la page
+// ======================================
+
+document.addEventListener("DOMContentLoaded", async function () {
+
+  await initHeader();
+  await initFooter();
+
+});
